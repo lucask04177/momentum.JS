29add7e2afc91df1c1369fe8fab5d32f51de27f7
Lucas Kang <97434133+lucask04177@users.noreply.github.com>
2022-08-16 23:30:57 +0900

    Re start!

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
-const age = parseInt (prompt("How old Are you?"));
+// // 220816 
+// Text = "hello" must use [""]
+// "hello" + "My name is Lucas"
+// String = From begining to end all Text
 
 
-if (isNaN(age) || age < 0) {
-    prompt("Please write a real positive Number");
-} else if(age < 18){
-    alert("You are too young");
-} else if(age >= 18 && age <= 50) {
-    alert("your can Drink");
-} else if(age) {
-    alert("You are Too old"); 
-}
+
+// // const age = parseInt (prompt("How old Are you?"));
+
+
+// // if (isNaN(age) || age < 0) {
+// //     prompt("Please write a real positive Number");
+// // } else if(age < 18){
+// //     alert("You are too young");
+// // } else if(age >= 18 && age <= 50) {
+// //     alert("your can Drink");
+// // } else if(age) {
+// //     alert("You are Too old"); 
+// // }
